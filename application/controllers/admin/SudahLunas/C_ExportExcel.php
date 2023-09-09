265ede960627c571529c4bcbf4f337c2870f2b0f
Dimas Bagus Wahyu Sudrajat <47965622+dimas150798@users.noreply.github.com>
2023-09-09 09:33:45 +0700

    Memperbarui Tanggal transaksi sudah lunas

--- application/controllers/admin/SudahLunas/C_ExportExcel.php
+++ application/controllers/admin/SudahLunas/C_ExportExcel.php
@@ -191,7 +191,7 @@ class C_ExportExcel extends CI_Controller
             $sheet->setCellValue('A' . $row_number, $key + 1);
             $sheet->setCellValue('B' . $row_number, $row['order_id']);
             $sheet->setCellValue('C' . $row_number, $tahunGET . '-' . $bulanGET . '-' .  $row['tanggal']);
-            $sheet->setCellValue('D' . $row_number, $row['created_at']);
+            $sheet->setCellValue('D' . $row_number, $row['tahunTransaksi'] . '-' . $row['bulanTransaksi'] . '-' .  $row['tanggalTransaksi']);
             $sheet->setCellValue('E' . $row_number, strtoupper($row['name']));
             $sheet->setCellValue('F' . $row_number, strtoupper($row['nama_paket']));
             $sheet->setCellValue('G' . $row_number, $row['gross_amount']);
