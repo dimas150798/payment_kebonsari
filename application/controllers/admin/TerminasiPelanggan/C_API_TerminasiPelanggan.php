a773fd9cd42cd028602e4ab8b652f42e5bb43e8a
Dimas Bagus Wahyu Sudrajat <47965622+dimas150798@users.noreply.github.com>
2024-02-16 15:01:08 +0700

    memperbarui API data pelanggan terminasi

--- application/controllers/admin/TerminasiPelanggan/C_API_TerminasiPelanggan.php
+++ application/controllers/admin/TerminasiPelanggan/C_API_TerminasiPelanggan.php
@@ -11,19 +11,10 @@ if (!function_exists('changeDateFormat')) {
 
 class C_API_TerminasiPelanggan extends CI_Controller
 {
+
     public function ApiTerminasi()
     {
-        // Mengambil data terminasi pelanggan
-        $dataPelanggan = $this->M_TerminasiPelanggan->TerminasiPelanggan();
-
-        // Menyiapkan data untuk dikirim sebagai respons JSON
-        $responseData = array(
-            'DataPelanggan' => $dataPelanggan,
-        );
-
-        // Mengirim respons dalam format JSON
-        $this->output
-            ->set_content_type('application/json')
-            ->set_output(json_encode($responseData));
+        $request = $this->M_TerminasiPelanggan->TerminasiPelanggan();
+        echo json_encode($request);
     }
 }
